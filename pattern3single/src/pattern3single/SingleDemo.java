4af24bcebb69f66347648dfd651495dbad6e36ca
SAISMITHI <SAISMITHI@LAPTOP-H9ROK097>
2020-06-06 18:41:19 +0530

    commit

---
+++ pattern3single/src/pattern3single/SingleDemo.java
@@ -0,0 +1,9 @@
+package pattern3single;
+
+public class SingleDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton obj = Singleton.getInstance();
+	      obj.show();
+	}
+}
---
+++ pattern3single/src/pattern3single/Singleton.java
@@ -0,0 +1,13 @@
+package pattern3single;
+
+public class Singleton {
+	private static Singleton s = new Singleton();
+	   private Singleton(){}
+	   public static Singleton getInstance(){
+	      return s;
+	   }
+
+	   public void show(){
+	      System.out.println("It's a singleton pattern type");
+	   }
+}
